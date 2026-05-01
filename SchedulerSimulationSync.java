b341c274971d7dee2feb36d8c64bbef96bd26d77
joud54 <445052069@std.psau.edu.sa>
2026-05-01 19:01:35 +0300

    protect shared counters using Reentrantlock

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -51,7 +51,13 @@ class SharedResources {
     public static void incrementContextSwitch() {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: Multiple threads might read and write simultaneously!
-        contextSwitchCount++;
+        lock.lock();
+        try {
+            contextSwitchCount++;    
+        } finally{
+            lock.unlock();
+        }         //3
+       
     }
     
     // Method to increment completed process counter
