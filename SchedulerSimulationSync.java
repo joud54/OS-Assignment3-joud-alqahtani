b4d5f47f9de9e5b2ee16c623724e61ee5673fc44
joud54 <445052069@std.psau.edu.sa>
2026-05-01 23:37:32 +0300

    apply semaphore in runToCompletion method

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -209,6 +209,10 @@ class Process implements Runnable {
     public void runToCompletion() {
         // TODO: Similar synchronization needed here
         try {
+           SharedResources.cpuSemaphore.acquire();
+    
+    
+        try {
             System.out.println(Colors.BRIGHT_CYAN + "  ⚡ " + Colors.BOLD + Colors.CYAN + name + 
                               Colors.RESET + Colors.BRIGHT_CYAN + " is the last process, running to completion" + 
                               Colors.RESET + " [" + remainingTime + "ms]");
@@ -226,6 +230,13 @@ class Process implements Runnable {
         } catch (InterruptedException e) {
             System.out.println(Colors.RED + "  ✗ " + name + " was interrupted." + Colors.RESET);
         }
+        } catch (InterruptedException e1) {
+    
+            e1.printStackTrace();
+        }
+        finally{
+            SharedResources.cpuSemaphore.release();
+        }  //9
     }
     
     public String getName() {
